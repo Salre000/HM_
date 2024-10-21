1bbdda70b773ab3e5a0271a89cc1fda4f10098b3
miyatatakumi <mcd2376019@stu.o-hara.ac.jp>
2024-10-21 11:37:36 +0900

    仮の移動のスプリクトを作成

---
+++ HM_/Project/HM/Assets/takumi/CameraManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+// カメラの管理をするプログラム
+public class CameraManager : MonoBehaviour
+{
+    //　カメラの回転速度　（ラジアン）
+    [SerializeField]private float _cameraSpeed;
+
+    //プレイヤーのゲームオブジェクト
+    [SerializeField] private GameObject _player;
+
+    //プレイヤーとカメラの距離
+    [SerializeField] private float _range;
+
+    [SerializeField] private float _horizontal;
+    [SerializeField] private float _vertical;
+
+    private float _cameraPositionAngle = 3.14f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraOffset = new Vector3(0, 5, 0);
+        transform.LookAt(_player.transform.position+cameraOffset);
+
+        // 移動量と回転量を求める
+        _horizontal = Input.GetAxis("HorizontalR");
+        _vertical = Input.GetAxis("VerticalR");
+
+        if (_horizontal == 0 && _vertical == 0) return;
+
+        Vector3 _position = this.transform.position;
+
+        _cameraPositionAngle -=( (_horizontal)/3.14f*180)*0.0001f;
+
+        _range += (_vertical)*0.1f;
+
+        //レンジの最小値を設定する
+        if (_range < 50.0f) { _range = 50.0f; }
+
+        //レンジの最大値を設定する
+        if (_range > 100) {  _range = 100; }
+
+
+
+        _position.x = _player.transform.position.x + Mathf.Sin(_cameraPositionAngle) * _range;
+        _position.z = _player.transform.position.z + Mathf.Cos(_cameraPositionAngle) * _range;
+
+        this.transform.position = _position;    
+
+
+
+
+
+
+
+    }
+}
--- HM_/Project/HM/Assets/takumi/PlayerMove.cs
+++ HM_/Project/HM/Assets/takumi/PlayerMove.cs
@@ -6,19 +6,35 @@ public class PlayerMove : MonoBehaviour
 {
     Vector3 PlayerPosition;
 
-    private float _horizontal;
-    private float _vertical;
+   [SerializeField]  private float _horizontal;
+    [SerializeField] private float _vertical;
+
+
+    //プレイヤーの角度
+    [SerializeField] private float _angle;
+
+    private PlayerStatus _status;
+
+
+    public Vector3 pos;
     // Start is called before the first frame update
     void Start()
     {
         //座標を今の座標に更新するプログラム
         PlayerPosition=this.transform.position;
 
+        this.gameObject.AddComponent<PlayerStatus>();
+
+        _status = this.GetComponent<PlayerStatus>();
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
+
+        pos = Vector3.zero;
         _horizontal = _vertical = 0;
 
         // 移動量と回転量を求める
@@ -29,12 +45,24 @@ public class PlayerMove : MonoBehaviour
         if (_horizontal == 0 && _vertical == 0) return;
 
 
-        Vector3 position=this.transform.position;
+        _angle -= (_horizontal) * 0.1f;
 
-        position.x += _horizontal;
-        position.z += _vertical;
+        Vector3 Angles = this.transform.eulerAngles;
+
+        Angles.y = _angle;
+        this.transform.eulerAngles = Angles;
 
 
+        pos=this.transform.position;
+
+
+        //プレイヤーの移動
+        //pos.x += ( _vertical * _status.GetSpeed());
+        pos.z += (_vertical * _status.GetSpeed());
+
+
+
+        this.transform.position=pos;
 
 
 
