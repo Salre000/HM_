5c308d8784192f787c37421995b4a91d470d3c38
Shuri <mcd2376046@stu.o-hara.ac.jp>
2024-11-11 17:17:01 +0900

    UI オプション

---
+++ HM_/Project/HM/Assets/Shuri/InputManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] TextAsset _text;
+
+    static InputManager instance;
+
+    public struct UseKey
+    {
+        public string a;
+        public string b;
+        public string x;
+        public string y;
+        public string start;
+        public string back;
+        public string rt;
+        public string lt;
+        public string rb;
+        public string lb;
+    }
+
+    public UseKey config;
+
+    private void Start()
+    {
+        if (instance != null) Destroy(this.gameObject);
+
+        instance = this;
+
+        DontDestroyOnLoad(this.gameObject);
+
+        string jsonText = _text.ToString();
+
+        JsonNode json = JsonNode.Parse(jsonText);
+
+        config.a = json["a"].Get<string>();
+        config.b = json["b"].Get<string>();
+        config.x = json["x"].Get<string>();
+        config.y = json["y"].Get<string>();
+        config.start = json["start"].Get<string>();
+        config.back = json["back"].Get<string>();
+        config.rt = json["rt"].Get<string>();
+        config.lt = json["lt"].Get<string>();
+        config.rb = json["rb"].Get<string>();
+        config.lb = json["lb"].Get<string>();
+    }
+}
---
+++ HM_/Project/HM/Assets/Shuri/OptionManager.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionManager : MonoBehaviour
+{
+    public int menuIndex = 1;
+    int menuNum = 2;
+
+    public int sliderIndex;
+
+    [SerializeField] GameObject uiPanel;
+    [SerializeField] GameObject beltText;
+    [SerializeField] GameObject objective;
+
+    [SerializeField] Slider[] slider;
+    [SerializeField] RectTransform cursor;
+
+    InputManager _inputManager;
+
+    
+
+    void Start()
+    {
+        uiPanel.SetActive(false);
+
+        _inputManager = GetComponent<InputManager>();
+    }
+
+    void Update()
+    {
+        // オプション画面の開閉
+        if (Input.GetKeyDown(_inputManager.config.start))
+        {
+            uiPanel.SetActive(!uiPanel.activeSelf);
+        }
+
+        // オプション画面が開いていたら
+        if(uiPanel.activeSelf)
+        {
+            // RB
+            if (Input.GetKeyDown(_inputManager.config.rb) && menuIndex < menuNum) 
+            {
+                menuIndex++;
+                StartCoroutine(UIMove(Vector3.left));
+            }
+            // LB
+            if (Input.GetKeyDown(_inputManager.config.lb) && menuIndex > 1) 
+            {
+                menuIndex--;
+                StartCoroutine(UIMove(Vector3.right));
+            }
+        }
+
+        switch(menuIndex)
+        {
+            case 1:break;
+                case 2:Option(); break;
+                case 3:break;
+        }
+    }
+
+    IEnumerator UIMove(Vector3 dir)
+    {
+        for (int i = 0; i < 20; i++) 
+        {
+            beltText.transform.position += dir * 250 / 20;
+            objective.transform.position += dir * 1500 / 20;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    void Option()
+    {
+        if (Input.GetAxis("D_Pad_V") > 0) sliderIndex++;
+        if (Input.GetAxis("D_Pad_V") < 0) sliderIndex--;
+
+        if (sliderIndex > slider.Length - 1) sliderIndex = 0;
+        if (sliderIndex < 0) sliderIndex = slider.Length - 1;
+
+        cursor.anchoredPosition = new Vector2(cursor.anchoredPosition3D.x, (1 - sliderIndex) * 100);
+
+        slider[sliderIndex].value += Input.GetAxis("D_Pad_H");
+    }
+}
