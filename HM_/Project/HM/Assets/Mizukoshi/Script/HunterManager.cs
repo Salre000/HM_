c1d9fe8e5bddf7e34b3bf45f0f399b3fa7ea8a3d
KaikiMizukoshi <mcd2376086@stu.o-hara.ac.kp>
2024-11-14 10:11:27 +0900

    HunterManagerクラスの実装

---
+++ HM_/Project/HM/Assets/Mizukoshi/Script/HunterManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterManager : MonoBehaviour
+{
+
+    private GameObject[] gameObjects;
+
+    int deathCount = 0;
+
+    Vector3 respawnPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObjects = GameObject.FindGameObjectsWithTag("Hunter");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            if (gameObjects[i].GetComponent<HunterHPManager>().isDeadFlag)
+            {
+
+            }
+        }
+    }
+
+    public int GetHunterDeathAmount()
+    {
+        return deathCount;
+    }
+
+    void Respawn(int i)
+    {
+        deathCount++;
+        gameObjects[i].GetComponent<HunterHPManager>().hp = 100;
+        gameObjects[i].GetComponent<HunterHPManager>().isDeadFlag = false;
+        gameObjects[i].transform.position = respawnPosition;
+    }
+}
