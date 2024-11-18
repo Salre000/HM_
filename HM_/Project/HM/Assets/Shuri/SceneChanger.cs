fcb597a11784d44a32b4007f920a2813f8d0029a
miyatatakumi <mcd2376019@stu.o-hara.ac.jp>
2024-11-18 12:22:06 +0900

    test

---
+++ HM_/Project/HM/Assets/Shuri/SceneChanger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] string sceneName;
+    string _nowScene;
+
+    readonly string title = "Title";
+    readonly string select = "Select";
+    readonly string main = "Main";
+    readonly string result = "Result";
+
+    private void Start()
+    {
+        _nowScene = SceneManager.GetActiveScene().name;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.JoystickButton3) && _nowScene == title) 
+        {
+            ChangeScene();
+        }
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ HM_/Project/HM/Assets/Shuri/SelectButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectButton : MonoBehaviour
+{
+    [SerializeField]Button button;
+    InputManager _inputManager;
+
+    void Start()
+    {
+        _inputManager = GameObject.Find("InputManager").GetComponent<InputManager>();
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("D_Pad_V") > 0)
+        {
+            button.navigation.selectOnUp.Select();
+            button = button.navigation.selectOnUp.GetComponent<Button>();
+        }
+        if (Input.GetAxis("D_Pad_V") < 0)
+        {
+            button.navigation.selectOnDown.Select();
+            button = button.navigation.selectOnDown.GetComponent<Button>();
+        }
+    }
+}
