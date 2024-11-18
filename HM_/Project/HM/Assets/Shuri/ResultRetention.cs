69bc5256a8ee5dddf62b199cc6cda6d842eed698
Shuri <mcd2376046@stu.o-hara.ac.jp>
2024-11-18 12:42:03 +0900

    消えた部分の修正

---
+++ HM_/Project/HM/Assets/Shuri/ResultRetention.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultRetention : MonoBehaviour
+{
+    float elapsedTime;
+    bool clearFlag;
+
+    static ResultRetention resultRetention;
+
+    void Start()
+    {
+        if (resultRetention == null)
+        {
+            resultRetention = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else Destroy(this.gameObject);
+
+        if (!(SceneManager.GetActiveScene().name == "Result" || SceneManager.GetActiveScene().name == "Main")) Destroy(this.gameObject);
+
+        TextMeshProUGUI resultTime = GameObject.Find("Time").GetComponent<TextMeshProUGUI>();
+
+        if (clearFlag) resultTime.text = "Time : " + elapsedTime.ToString();
+        else resultTime.text = "Time : --:--";
+    }
+
+    public void SetResultData(bool flag, float time)
+    {
+        clearFlag = flag;
+        elapsedTime = time;
+    }
+}
