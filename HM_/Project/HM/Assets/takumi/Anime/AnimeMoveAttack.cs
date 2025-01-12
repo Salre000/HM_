f7d846c55197987d3539a2118adc9dc9cc6a9fc8
SALRE000\tashi <Salre>
2025-01-12 14:46:17 +0900

    移動攻撃の当たり判定を独立した方法に変更

--- HM_/Project/HM/Assets/takumi/Anime/AnimeMoveAttack.cs
+++ HM_/Project/HM/Assets/takumi/Anime/AnimeMoveAttack.cs
@@ -4,12 +4,8 @@ using UnityEngine;
 
 public class AnimeMoveAttack : AnimeBase
 {
-    //あたり判定を生成するオブジェクト
-    GameObject[] HitGameObject = new GameObject[3];
 
-    SphereCollider[] Sphere = new SphereCollider[3];
 
-    Damage[] damage = new Damage[3];
 
     string NestName= "Armature|AttackMoveLoops";
 
@@ -18,28 +14,6 @@ public class AnimeMoveAttack : AnimeBase
     {
 
         _AnimeName = "Armature|AttackMove";
-
-        HitGameObject[0] = GameObject.Find("Bone.024");
-        HitGameObject[1] = GameObject.Find("Bone.019");
-        HitGameObject[2] = GameObject.Find("Bone.022");
-
-
-
-        for (int i = 0; i < 3; i++)
-        {
-            Sphere[i] = HitGameObject[i].AddComponent<SphereCollider>();
-
-            //HitGameObject[i].tag;
-            Sphere[i].radius = 0.01f;
-            Sphere[i].center = Vector3.zero;
-            Sphere[i].isTrigger = true;
-            HitGameObject[i].tag =TagBox.GetPlayerAttackTag();
-
-            damage[i] = HitGameObject[i].AddComponent<Damage>();
-            damage[i].SetDamage(1);
-
-        }
-
     }
     private void FixedUpdate()
     {
@@ -59,15 +33,6 @@ public class AnimeMoveAttack : AnimeBase
 
         AnimeMoveAttack jumpAnime = this.gameObject.GetComponent<AnimeMoveAttack>();
 
-
-        for (int i = 0; i < 3; i++)
-        {
-            HitGameObject[i].tag = TagBox.GetPlayerTag();
-
-            Destroy(Sphere[i]);
-            Destroy(damage[i]);
-
-        }
         Destroy(jumpAnime);
 
 
