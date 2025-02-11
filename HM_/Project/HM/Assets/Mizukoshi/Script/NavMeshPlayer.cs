4dcbe6ac64d4f60f7863d90213b3a80474194a3f
Ka-Mizu-Iki <mcd2376086@stu.o-hara.ac.jp>
2025-02-11 23:19:41 +0900

    ハンターの音を鳴らす関数をまとめたHunterSoundクラスの実装 報告　おそらくNavmeshのBakeの失敗によりHunterが動かなくなっている

--- HM_/Project/HM/Assets/Mizukoshi/Script/NavMeshPlayer.cs
+++ HM_/Project/HM/Assets/Mizukoshi/Script/NavMeshPlayer.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavMeshPlayer : MonoBehaviour
 {
+
+    NavMeshAgent nav;
+    public GameObject g;
     // Start is called before the first frame update
     void Start()
     {
-        
+        nav=GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        nav.destination = g.transform.position;
     }
 }
