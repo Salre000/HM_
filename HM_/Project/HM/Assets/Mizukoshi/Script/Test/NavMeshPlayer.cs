71199e0da8607b8b7a2f293fcb54470dde6d4684
KaikiMizukoshi <mcd2376086@stu.o-hara.ac.kp>
2025-02-14 14:13:35 +0900

    no message

--- HM_/Project/HM/Assets/Mizukoshi/Script/Test/NavMeshPlayer.cs
+++ HM_/Project/HM/Assets/Mizukoshi/Script/Test/NavMeshPlayer.cs
@@ -8,18 +8,26 @@ public class NavMeshPlayer : MonoBehaviour
 
     NavMeshAgent nav;
     private GameObject g;
+    NavMeshHit hit;
+    private Vector3 vector;
+   
     // Start is called before the first frame update
     void Start()
     {
         nav=GetComponent<NavMeshAgent>();
-        g = GameObject.Find("Cubes");
+        //g = GameObject.Find("Cubes");
+        
+        g = GameObject.FindWithTag("Player");
 
-        //g = GameObject.FindWithTag("Player");
+
+        nav.SetDestination(g.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        nav.SetDestination(g.transform.position);
+       
+        nav.destination=g.transform.position;
+    
     }
 }
