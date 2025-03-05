90770b53e4a8abd212f286474570f5303fb4a423
KaikiMizukoshi <mcd2376086@stu.o-hara.ac.kp>
2025-03-05 11:35:42 +0900

    矢のリスト化に成功

--- HM_/Project/HM/Assets/Mizukoshi/Script/Test/MoveAnddestory.cs
+++ HM_/Project/HM/Assets/Mizukoshi/Script/Test/MoveAnddestory.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class MoveAnddestory : MonoBehaviour
 {
+    private float t;
     private void Update()
     {
-       
+        if (this.gameObject.activeSelf)
+        {
+            t += Time.deltaTime;
+            if (t > 5.0f)
+            {
+                t = 0;
+                this.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            t = 0;
+        }
     }
-    private void OnTriggerEnter(Collider other)
-    {
-        this.gameObject.SetActive(false);
-    }
+  
 }
